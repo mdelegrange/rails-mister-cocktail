11db96224e7207a9e1db89718f46210c45c53222
matthieu delegrange <mdelegrange@gmail.com>
2018-11-22 14:37:58 +0100

    Routing, Controller, Views for Doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    redirect_to cocktail_path(@cocktail)
+
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
