fbcff5bd87a5c7a3aaa78f49c4347df2c8d69962
matthieu delegrange <mdelegrange@gmail.com>
2018-11-22 17:42:42 +0100

    Updated dose destroy method

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,14 +7,14 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose.cocktail = @cocktail
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose.ingredient = @ingredient
 
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'cocktails/show'
     end
   end
 
