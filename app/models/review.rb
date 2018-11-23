e3377ae41938d3205d8c3791f5c1e0db697bf7d7
matthieu delegrange <mdelegrange@gmail.com>
2018-11-23 16:17:37 +0100

    Added Reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
-  validates :content, :rating, presence: true
+  validates :content, presence: true
+  validates :rating,
+            presence: true,
+            numericality: { only_integer: true },
+            inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
